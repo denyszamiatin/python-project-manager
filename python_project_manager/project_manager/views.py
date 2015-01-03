f5768a2efe5b462c090ce0ed2f94636bf690d32d
Oleksandr Horobets <alex.v.gorobets@gmail.com>
2015-01-03 14:43:41 +0200

    Issue 14: project settings

--- python_project_manager/project_manager/views.py
+++ python_project_manager/project_manager/views.py
@@ -1,6 +1,3 @@
-import os.path, sys
-import json
-
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, render_to_response, redirect
 from django.template.loader import get_template
@@ -39,6 +36,37 @@ def project(request, project_id="0"):
 	menu = pm_menu('project', request, {'project': project})
 	return render_to_response('project_model/project_page_template.html',
 							  {'current_project': project, 'projects': projects, 'menu': menu}, context)
+
+
+@login_required()
+def project_settings(request, id="0"):
+    context = RequestContext(request)
+    project = Project.objects.filter(id = id)[0]
+    projects = UserRole.objects.all().filter(user = request.user)
+
+    if project.owner != request.user.username:
+        messages.error(request, "You don't have a permission to view this project!")
+
+        return HttpResponseRedirect('/main/')
+
+    if request.method == 'POST':
+        project_form = ProjectForm(data=request.POST)
+        if project_form.is_valid():
+            updated_project = project_form.instance
+            updated_project.id = id
+            updated_project.save()
+
+            messages.info(request, "Project settings updated!")
+            return HttpResponseRedirect('/main/')
+        else:
+            messages.error(request, "Failed to save project settings!")
+    else:
+        project_form = ProjectForm(instance = project)
+
+    return render_to_response('project_model/project_settings_template.html',
+                              {'current_project': project,
+                               'projects': projects,
+                               'project_form': project_form}, context)
 
 
 def user_login(request):
