d070b29c42105bfa2c6f09ab41d8c28e62684bff
ArthurFeschenko <helion875215@gmail.com>
2015-01-08 17:53:14 +0200

    27 created logout button for some navbars coreated related name for users in task model fixed navbar with no username deleted project manager folder... again made a task migration to database

--- python_project_manager/python_project_manager/settings.py
+++ python_project_manager/python_project_manager/settings.py
@@ -124,7 +124,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"), 'python_project_manager/project_manager/static/')
+STATICFILES_DIRS = ('python_project_manager/project_manager/static/', )
 
 STATICFILES_FINDERS = {
 	'django.contrib.staticfiles.finders.FileSystemFinder',
